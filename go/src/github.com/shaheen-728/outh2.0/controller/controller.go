19dedec50436cb782db5342e04fbf29f97332997
shaheen728 <shaheen@appointy.com>
2023-02-13 12:16:20 +0530

    add project

---
+++ go/src/github.com/shaheen-728/outh2.0/controller/controller.go
@@ -0,0 +1,78 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+)
+
+func GoogleCallback(w http.ResponseWriter, r *http.Request) {
+	// check is method is correct
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// get oauth state from cookie for this user
+	oauthState, _ := r.Cookie("oauthstate")
+	state := r.FormValue("state")
+	code := r.FormValue("code")
+	w.Header().Add("content-type", "application/json")
+
+	// ERROR : Invalid OAuth State
+	if state != oauthState.Value {
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		fmt.Fprintf(w, "invalid oauth google state")
+		return
+	}
+
+	// Exchange Auth Code for Tokens
+	token, err := config.AppConfig.GoogleLoginConfig.Exchange(
+		context.Background(), code)
+
+	// ERROR : Auth Code Exchange Failed
+	if err != nil {
+		fmt.Fprintf(w, "falied code exchange: %s", err.Error())
+		return
+	}
+
+	// Fetch User Data from google server
+	response, err := http.Get(config.OauthGoogleUrlAPI + token.AccessToken)
+
+	// ERROR : Unable to get user data from google
+	if err != nil {
+		fmt.Fprintf(w, "failed getting user info: %s", err.Error())
+		return
+	}
+
+	// Parse user data JSON Object
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Fprintf(w, "failed read response: %s", err.Error())
+		return
+	}
+
+	// send back response to browser
+	fmt.Fprintln(w, string(contents))
+}
+
+func GoogleLogin(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Create oauthState cookie
+	oauthState := utils.GenerateStateOauthCookie(w)
+	/*
+		AuthCodeURL receive state that is a token to protect the user
+		from CSRF attacks. You must always provide a non-empty string
+		and validate that it matches the the state query parameter
+		on your redirect callback.
+	*/
+	u := config.AppConfig.GoogleLoginConfig.AuthCodeURL(oauthState)
+	http.Redirect(w, r, u, http.StatusTemporaryRedirect)
+}
---
+++ go/src/github.com/shaheen-728/outh2.0/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+
+)
+
+func main() {
+	// load configs
+	config.LoadEnv()
+	config.LoadConfig()
+
+	// create a router
+	mux := http.NewServeMux()
+
+	// define routes
+	mux.HandleFunc("/google_login", controller.GoogleLogin)
+	mux.HandleFunc("/google_callback", controller.GoogleCallback)
+
+	// run server
+	log.Println("started server on :: http://localhost:8080/")
+	if oops := http.ListenAndServe(":8080", mux); oops != nil {
+		log.Fatal(oops)
+	}
+}
